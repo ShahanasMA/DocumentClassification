d066ad8a63aa574b9d6d9e361a9001e674923480
ShahanasMA <chn24mc052@ceconline.edu>
2026-03-12 11:13:40 +0530

    Create users.py

---
+++ users.py
@@ -0,0 +1,12 @@
+from werkzeug.security import generate_password_hash
+
+USERS = {
+    "admin": {
+        "password": generate_password_hash("admin123"),
+        "role": "admin"
+    },
+    "user": {
+        "password": generate_password_hash("user123"),
+        "role": "user"
+    }
+}
